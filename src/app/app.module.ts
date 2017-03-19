e82220b16f2306fba9b0004449178ffe483408bb
okachynskyy <oleksiy.kachynskyy@gmail.com>
2017-03-19 10:21:34 +0200

    Add home component, add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
     CoreModule
   ],
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
