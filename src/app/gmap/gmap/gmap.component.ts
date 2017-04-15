3c7bb00a51bd1228ca8122171a2b7494656e68a5
okachynskyy <oleksiy.kachynskyy@gmail.com>
2017-04-15 18:26:41 +0300

    Add initializing overlays and seeting map by callback

--- src/app/gmap/gmap/gmap.component.ts
+++ src/app/gmap/gmap/gmap.component.ts
@@ -1,3 +1,5 @@
+/// <reference path='../../../../node_modules/@types/google-maps/index.d.ts' />
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,18 +8,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gmap.component.scss']
 })
 export class GmapComponent implements OnInit {
-
+  
   options: any;
 
   overlays: any[];
+
+  map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
     this.options = {
       center: { lat: 49.8397, lng: 24.0297 },
-      zoom: 12
+      zoom: 14
     };
+    
+    this.initOverlays();
+  }
+
+  initOverlays() {
+    if (!this.overlays || !this.overlays.length) {
+      this.overlays = [
+        new google.maps.Marker({ position: { lat: 49.840611, lng: 24.022510 } }),
+        new google.maps.Marker({ position: { lat: 49.844065, lng: 24.026242 } }),
+        new google.maps.Marker({ position: { lat: 49.837548, lng: 24.030172 } }),
+        new google.maps.Marker({ position: { lat: 49.842089, lng: 24.016398 } }),
+        new google.maps.Marker({ position: { lat: 49.836553, lng: 24.004382 } })
+      ];
+    }
+  }
+
+  setMap(event) {
+    this.map = event.map;
+    console.log('setMap', event.map);
+  }
+
+  handleMapClick(event) {
+    this.addMarker(event.latLng);
+  }
+
+  addMarker(latLng) {
+    console.log(this.overlays);
+    
+    this.overlays.push(new google.maps.Marker({
+      position: {
+        lat: latLng.lat(),
+        lng: latLng.lng()
+      },
+      draggable: false
+    }));
+  }
+
+  buildRoute() {
+    console.log(this.map);
   }
 
 }
